43658d7ebef68b2706382b2e7bbc8e093f6de491
rainbow7174 <rainbow7174@gmail.com>
2015-01-23 15:48:57 +0900

    Revert "source"
    This reverts commit 9bdb4d859d6c07d619f0ce8d090d700f7b7b88b1.

--- gen/kr/hs2/co/form/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package kr.hs2.co.form;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/kr/hs2/co/util/FileUtil.java
+++
@@ -1,7 +0,0 @@
-package kr.hs2.co.util;
-
-public class FileUtil {
-	public static String getExtension(String fileStr){
-		return fileStr.substring(fileStr.lastIndexOf(".")+1,fileStr.length());
-	}
-}
